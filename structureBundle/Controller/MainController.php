d5380e3127d86960c000178e92ec367074601261
yoshi <josien-lionel@hotmail.fr>
2014-04-26 02:48:12 +0200

    ajout des pages manquantes

--- structureBundle/Controller/MainController.php
+++ structureBundle/Controller/MainController.php
@@ -4,6 +4,8 @@ namespace Toilettage\structureBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Toilettage\structureBundle\Entity\KeywordsRepo;
+use Yomaah\ajaxBundle\Controller\AjaxController;
+use Yomaah\structureBundle\Entity\MyMail;
 
 class MainController extends Controller
 {
@@ -11,6 +13,28 @@ class MainController extends Controller
     {
         $params = $this->getParams('toilettage_accueil');
         return $this->render('ToilettagestructureBundle:Main:accueil.html.twig', $params);
+    }
+
+    public function toilettageAction()
+    {
+        $params = $this->getParams('toilettage_toilettage');
+        return $this->render('ToilettagestructureBundle:Main:toilettage.html.twig', $params);
+    }
+
+    public function galerieAction()
+    {
+        $params = $this->getParams('toilettage_galerie');
+        $params['images'] = AjaxController::imageSearch('galerie/active', 'Toilettage/structureBundle');
+        return $this->render('ToilettagestructureBundle:Main:galerie.html.twig', $params);
+    }
+
+    private function getForm($mail)
+    {
+        $formBuilder = $this->createFormBuilder($mail);
+        $formBuilder->add('Objet','text',array('attr' => array ('placeholder' => 'L\'objet de votre message'),'label' => 'Sujet de votre message :'))
+                    ->add('From','email',array('attr' => array ('placeholder' => 'Votre adresse email'),'label' => 'Votre email :'))
+                    ->add('Message','textarea',array('attr' => array ('placeholder' => 'Votre message ...'),'label' => 'Votre message :'));
+        return $formBuilder->getForm();
     }
     public function contactAction()
     {
@@ -28,7 +52,7 @@ class MainController extends Controller
                 $mail =  new MyMail();
                 $form = $this->getForm($mail);
                 $this->get('session')->set('envoie', true);
-                return $this->redirect($this->generateUrl('literie_contact'));
+                return $this->redirect($this->generateUrl('toilettage_contact'));
             }
         }
         if ($this->get('session')->has('envoie'))
@@ -36,13 +60,23 @@ class MainController extends Controller
             $this->get('session')->remove('envoie');
             $envoi = true;
         }
-        $params = $this->getParams('literie_contact');
-        $h = new HoraireRepo();
-        $params['horaires'] = $h->getHoraires();
+        $params = $this->getParams('toilettage_contact');
+        $tmp = array();
+        foreach ($params['articles'] as $article)
+        {
+            if ($article->getTagName() == 'adresse_courrier' || $article->getTagName() == 'adresse_phone')
+            {
+                $tmp[] = $article;
+            }
+        }
+        $params['adresse'] = $tmp;
+        //$h = new HoraireRepo();
+        //$params['horaires'] = $h->getHoraires();
         $params['form'] = $form->createView();
         $params['envoie'] = $envoi;
-        return $this->get('templating')->renderResponse('EuroLiteriestructureBundle:Main:contact.html.twig', $params);
+        return $this->get('templating')->renderResponse('ToilettagestructureBundle:Main:contact.html.twig', $params);
     }
+
     private function getParams($page)
     {
         $dispatcher = $this->get('bundleDispatcher');
--- structureBundle/Resources/config/routing.php
+++ structureBundle/Resources/config/routing.php
@@ -9,14 +9,14 @@ $collection->add('toilettage_accueil', new Route('/toilettage/accueil', array(
     '_controller' => 'ToilettagestructureBundle:Main:accueil',
 )));
 
-$collection->add('toilettage_toilettage', new Route('/toilettage', array(
-    '_controller' => 'ToilettagestructureBundle:Main:accueil',
+$collection->add('toilettage_toilettage', new Route('/toilettage/toilettage', array(
+    '_controller' => 'ToilettagestructureBundle:Main:toilettage',
 )));
 $collection->add('toilettage_accessoire', new Route('/toilettage', array(
     '_controller' => 'ToilettagestructureBundle:Main:accueil',
 )));
-$collection->add('toilettage_photo', new Route('/toilettage', array(
-    '_controller' => 'ToilettagestructureBundle:Main:accueil',
+$collection->add('toilettage_photo', new Route('/toilettage/photos', array(
+    '_controller' => 'ToilettagestructureBundle:Main:galerie',
 )));
 $collection->add('toilettage_contact', new Route('/toilettage/contact', array(
     '_controller' => 'ToilettagestructureBundle:Main:contact',
